2e9f286f7a93a11a19378811ff8d015d2368583c
Dennis Daume <daume.dennis@gmail.com>
2013-02-02 14:47:57 +0100

    Extended DisposeWith

--- Marrow/Extensions.cs
+++ Marrow/Extensions.cs
@@ -1,12 +1,16 @@
 ﻿using System;
+using System.Diagnostics.Contracts;
 using System.Reactive.Disposables;
 
 namespace Marrow
 {
     public static class Extensions
     {
-        public static T DisposeWith<T>(this T disposable, CompositeDisposable with) where T : IDisposable
+        public static T DisposeWith<T>(this T disposable, CompositeDisposable with) where T : class, IDisposable
         {
+            Contract.Requires(disposable != null);
+            Contract.Requires(with != null);
+
             with.Add(disposable);
 
             return disposable;
