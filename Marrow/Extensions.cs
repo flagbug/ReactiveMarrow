717d320c765ee7317b111418539d133b42c93ebb
Dennis Daume <daume.dennis@gmail.com>
2013-02-02 14:36:33 +0100

    Added DisposeWith extension method

---
+++ Marrow/Extensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Reactive.Disposables;
+
+namespace Marrow
+{
+    public static class Extensions
+    {
+        public static T DisposeWith<T>(this T disposable, CompositeDisposable with) where T : IDisposable
+        {
+            with.Add(disposable);
+
+            return disposable;
+        }
+    }
+}
