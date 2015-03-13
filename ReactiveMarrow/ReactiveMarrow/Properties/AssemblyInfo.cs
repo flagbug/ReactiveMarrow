6334311f7b654310208a4ca9812f8707e5c85393
Dennis Daume <daume.dennis@gmail.com>
2015-03-13 12:07:49 +0100

    Release of version 0.5.1

--- ReactiveMarrow/ReactiveMarrow/Properties/AssemblyInfo.cs
+++ ReactiveMarrow/ReactiveMarrow/Properties/AssemblyInfo.cs
@@ -8,16 +8,16 @@ using System.Resources;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ReactiveMarrow")]
-[assembly: AssemblyCopyright("Copyright © Dennis Daume 2014")]
+[assembly: AssemblyCopyright("Copyright © Dennis Daume 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
 
 // Version information for an assembly consists of the following four values:
-//
+// 
 // Major Version Minor Version Build Number Revision
-//
+// 
 // You can specify all the values or you can default the Build and Revision Numbers by using the '*'
 // as shown below: [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.5.0")]
-[assembly: AssemblyFileVersion("0.5.0")]
+[assembly: AssemblyVersion("0.5.1")]
+[assembly: AssemblyFileVersion("0.5.1")]
